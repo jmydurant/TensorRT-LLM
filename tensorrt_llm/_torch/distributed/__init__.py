2122180dd748826e4b47e2857021211b4e000780
Mingyang Jiang <13463932+jmydurant@users.noreply.github.com>
2026-02-12 14:20:28 +0800

    fix: bug fix for unit test
    Signed-off-by: Mingyang Jiang <13463932+jmydurant@users.noreply.github.com>

--- tensorrt_llm/_torch/distributed/__init__.py
+++ tensorrt_llm/_torch/distributed/__init__.py
@@ -3,9 +3,9 @@ from tensorrt_llm.functional import AllReduceFusionOp
 from .communicator import Distributed, MPIDist, TorchDist
 from .moe_alltoall import MoeAlltoAll
 from .ops import (AllReduce, AllReduceParams, AllReduceStrategy,
-                  HelixAllToAllNative, MoEAllReduce, MoEAllReduceParams,
-                  allgather, alltoall_helix, cp_allgather, reducescatter,
-                  userbuffers_allreduce_finalize)
+                  HelixAllToAllNative, MiniMaxAllReduceRMS, MoEAllReduce,
+                  MoEAllReduceParams, allgather, alltoall_helix, cp_allgather,
+                  reducescatter, userbuffers_allreduce_finalize)
 
 __all__ = [
     "allgather",
@@ -20,6 +20,7 @@ __all__ = [
     "HelixAllToAllNative",
     "MoEAllReduce",
     "MoEAllReduceParams",
+    "MiniMaxAllReduceRMS",
     "MoeAlltoAll",
     "TorchDist",
     "MPIDist",
