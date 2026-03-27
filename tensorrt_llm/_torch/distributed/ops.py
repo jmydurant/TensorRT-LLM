55739c24cca318fde5c76b336fc6aaca10dbae42
Mingyang Jiang <13463932+jmydurant@users.noreply.github.com>
2026-03-27 13:19:09 +0800

    feature: modify float4 kernel, split q, k to different warp, enable pdl
    Signed-off-by: Mingyang Jiang <13463932+jmydurant@users.noreply.github.com>

--- tensorrt_llm/_torch/distributed/ops.py
+++ tensorrt_llm/_torch/distributed/ops.py
@@ -1193,7 +1193,7 @@ class MiniMaxAllReduceRMS(nn.Module):
                                                       self.workspace,
                                                       self.mapping.tp_rank,
                                                       self.mapping.tp_size, eps,
-                                                      False)
+                                                      True)
 
     def forward_qk(self, q: torch.Tensor, k: torch.Tensor,
                    rms_weights_q: torch.Tensor, rms_weights_k: torch.Tensor,
@@ -1208,6 +1208,6 @@ class MiniMaxAllReduceRMS(nn.Module):
             self.mapping.tp_rank,
             self.mapping.tp_size,
             eps,
-            False,
+            True,
         )
         return (out_list[0], out_list[1])
